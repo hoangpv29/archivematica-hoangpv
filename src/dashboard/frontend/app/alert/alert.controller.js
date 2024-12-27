f4a6afca7934b28249ba615a7360ffd1f479e3ea
ducphuongLoveC <vukhanhphuong2k4@gmail.com>
2024-12-27 10:33:35 +0700

    update

--- src/dashboard/frontend/app/alert/alert.controller.js
+++ src/dashboard/frontend/app/alert/alert.controller.js
@@ -2,11 +2,16 @@ import angular from 'angular';
 
 angular.module('alertController', ['alertService']).
 
-controller('AlertDisplayController', ['Alert', function(Alert) {
+controller('AlertDisplayController', ['Alert','$timeout', function(Alert) {
   var vm = this;
 
   vm.alert = Alert;
   vm.remove = index => {
     Alert.alerts.splice(index, 1);
   };
+  vm.clearAll = function() {
+    Alert.alerts = [];
+  };
+
+  
 }]);
